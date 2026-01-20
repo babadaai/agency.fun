dceec35dc98f78cc155bd58515a8d409c8f75f36
nischal <nischalmainali75@gmail.com>
2026-01-20 19:02:49 +0545

    final commit

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -22,8 +22,10 @@ const OurWork = () => {
 
   ]
   return (
-    <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 
+    <div id='our-work' className='flex flex-col
+     items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 
     pt-30 text-gray-700 dark:text-white overflow-hidden'>
+      
       <Title title='Our latest work' desc='From strategy to execution,
        we craft digital solutions that move your business forward.'/>
       <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
